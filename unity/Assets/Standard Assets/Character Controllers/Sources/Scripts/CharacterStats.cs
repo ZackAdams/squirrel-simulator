c2925de2bc3587b9a3eb52a52d6d994a12811d5f
Zack Adams <reqmor@gmail.com>
2014-03-21 23:34:39 -0500

    Added basic character stats

---
+++ unity/Assets/Standard Assets/Character Controllers/Sources/Scripts/CharacterStats.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterStats : MonoBehaviour {
+
+	public int nutCapacity = 3;
+	public int nutCount = 0;
+	public float maxHealth = 100;
+	public float health = 100;
+	
+}
