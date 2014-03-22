fa5c3a6289c296ea12ee42e799a293303401198c
Thomas Fritz <tommy.fritz@hitcents.com>
2014-03-22 01:32:21 -0500

    added script for controlling the splash title screen.

---
+++ unity/Assets/SplashController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashController : MonoBehaviour {
+
+	public GUITexture splashView;
+	public Texture2D splashTitle;
+	public Texture2D splashStartGame;
+	public GUIText startGame;
+	public GUIText quitGame;
+
+	// Use this for initialization
+	void Start () {
+		splashView.texture = splashTitle;
+		Invoke("nextScene", 8);
+	}
+
+	void nextScene () {
+		splashView.texture = splashStartGame;
+		startGame.text = "Start the Game";
+		quitGame.text = "Quit Game";
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
