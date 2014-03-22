59353c2997cc228f16eedbb0402d712003872a4c
Thomas Fritz <tommy.fritz@hitcents.com>
2014-03-22 18:52:17 -0500

    adding gui text for hud.

---
+++ unity/Assets/HudDisplay.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class HudDisplay : MonoBehaviour {
+
+
+	public GUIText alertMessage;
+	public GUIText NutsCollected;
+	public GUIText Hunger;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
