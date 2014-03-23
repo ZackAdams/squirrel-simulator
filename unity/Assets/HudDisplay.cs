9563afb45c60f7dbf23a8ff13de537c1329bd913
Thomas Fritz <tommy.fritz@hitcents.com>
2014-03-22 19:29:54 -0500

    setup the hud to display from the script.

--- unity/Assets/HudDisplay.cs
+++ unity/Assets/HudDisplay.cs
@@ -6,16 +6,27 @@ public class HudDisplay : MonoBehaviour {
 
 
 	public GUIText alertMessage;
-	public GUIText NutsCollected;
-	public GUIText Hunger;
+	public GUIText nutsCollected;
+	public GUIText hunger;
 
 	// Use this for initialization
 	void Start () {
-	
+		alertMessage.text = "";
+		nutSetText (1);
+		hungerSetText ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+	void nutSetText(int nuts) {
+		nutsCollected.text = "Nuts: 1";
+	}
+
+	void hungerSetText () {
+		hunger.text = "Hunger: Very Hungry";
+	}
+
 }
