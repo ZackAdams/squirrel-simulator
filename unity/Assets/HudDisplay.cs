08d5a2fbb2c491606b705fd292d2007a44f56481
Thomas Fritz <tommy.fritz@hitcents.com>
2014-03-22 20:10:42 -0500

    made the timer on hud functional

--- unity/Assets/HudDisplay.cs
+++ unity/Assets/HudDisplay.cs
@@ -8,21 +8,36 @@ public class HudDisplay : MonoBehaviour {
 	public GUIText alertMessage;
 	public GUIText nutsCollected;
 	public GUIText hunger;
+	public GUIText displayTimer;
+	public float collectTimer;
 
 	// Use this for initialization
 	void Start () {
 		alertMessage.text = "";
 		nutSetText (1);
 		hungerSetText ();
+		collectTimer = 60.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		collectTimer -= Time.deltaTime;
+		setTimerText (collectTimer);
+		
+		if (collectTimer <= 0.0f)
+		{
+			Debug.Log("GAME OVER");
+		}
+		
+	}
 	
+
+	void setTimerText(float time) {
+		displayTimer.text = "Time: " + time.ToString ();
 	}
 
 	void nutSetText(int nuts) {
-		nutsCollected.text = "Nuts: 1";
+		nutsCollected.text = "Nuts: " + nuts.ToString();
 	}
 
 	void hungerSetText () {
